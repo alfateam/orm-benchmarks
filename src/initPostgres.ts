875a5cb6acfed77957bb69654ce166b67de89a1b
Lars-Erik Roald <lars.roald@systor.st>
2024-06-12 12:16:38 +0000

    init

--- src/initPostgres.ts
+++ src/initPostgres.ts
@@ -2,10 +2,12 @@ import path from 'node:path';
 import fs from 'node:fs';
 const sql = fs.readFileSync(path.resolve("./data/init-postgres.sql"), "utf-8");
 import db from './orange/postgres';
+import { exit } from 'node:process';
 
 
 async function init() {
     await db.query(sql);
+    exit(0);
 }
 
-export default init;
+init();
