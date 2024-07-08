76bdab0c0b090ee2ecb761fe8dc7293b07b88b60
Lars-Erik Roald <lars.roald@systor.st>
2024-07-08 06:35:42 +0000

    poolsize 10

--- src/orange/postgres.ts
+++ src/orange/postgres.ts
@@ -1,4 +1,4 @@
 import map from './map';
 
-export default map.postgres('postgres://postgres:postgres@postgres/postgres', { size: 1});
+export default map.postgres('postgres://postgres:postgres@postgres/postgres', { size: 10});
 
